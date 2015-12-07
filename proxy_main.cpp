5967ceeb24a0fab047cdc6f9a02061f7d8f6c674
ssaemo <jeontha@gmail.com>
2015-12-07 22:52:57 +0900

    good

--- proxy_main.cpp
+++ proxy_main.cpp
@@ -19,10 +19,10 @@
 #define PROTO_UDP 17
 #define PROTO_ICMP 1
 #define ICMPHDR_LEN 8
-#define MIDDLE_MAC {0x00, 0x0c, 0x29, 0x77, 0xe5, 0x7e} //(SRC)AP_IP/MAC -> MIDDLE_IP/MAC
-#define MIDDLE_IP "192.168.230.255"
-#define AP_MAC {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}									//(DST)MIDDLE_IP/MAC -> AP_IP/MAC
-#define AP_IP "192.168.230.164"
+#define MIDDLE_MAC {0xd8, 0xfc, 0x93, 0x46, 0x58, 0x70} //(SRC)AP_IP/MAC -> MIDDLE_IP/MAC
+#define MIDDLE_IP "192.168.32.231" //BOB
+#define AP_MAC {0x00, 0x27, 0x1c, 0xcd, 0xdd, 0x04}									//(DST)MIDDLE_IP/MAC -> AP_IP/MAC
+#define AP_IP "192.168.137.84" //BOB_MIL
 
 void req_handling(u_char *args, const struct pcap_pkthdr *header, const u_char *buffer)
 {
@@ -96,7 +96,7 @@ void req_handling(u_char *args, const struct pcap_pkthdr *header, const u_char *
 		printf("0x%08x\n", ipptr->saddr);
 	}
 
-	return; //stop
+	//return; //stop
 
 	/* Send down the packet */
 	if (pcap_sendpacket(res_handle, buffer, header->len /* size */) != 0)
